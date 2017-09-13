101feee9ddc9661ee5dee4a0293d8c7e1208d4ab
karpenberg <karpenberg@gmail.com>
2017-09-13 14:59:43 +0300

    removed
    removed

--- CSAuthorAngular2InASPNetCore/ClientApp/app/_model/RequestResult.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var RequestResult = (function () {
-    function RequestResult() {
-    }
-    return RequestResult;
-}());
-exports.RequestResult = RequestResult;
-//# sourceMappingURL=RequestResult.js.map
